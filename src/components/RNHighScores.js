6f985552f92230afddf3836e1e1fa3badcb8fbef
Changnam Hong <hcn1519@gmail.com>
2018-11-06 00:29:22 +0900

    edit component directory

---
+++ src/components/RNHighScores.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default class RNHighScores extends React.Component {
+  render() {
+    let contents = this.props['scores'].map((score) => (
+      <Text key={score.name}>
+        {score.name}:{score.value}
+        {'\n'}
+      </Text>
+    ));
+    return (
+      <View style={styles.container}>
+        <Text style={styles.highScoresTitle}>2048 High Scores!</Text>
+        <Text style={styles.scores}>{contents}</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#FFFFFF',
+  },
+  highScoresTitle: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  scores: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
