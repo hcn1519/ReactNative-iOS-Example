7e2543cc6754c58dfa63c48065640df7d280119b
changnam.hong <changnam.hong@navercorp.com>
2018-11-15 17:57:16 +0900

    bundle sync 맞춤

--- index.js
+++ index.js
@@ -1,17 +1,14 @@
 import React from 'react';
 import {AppRegistry, StyleSheet, Text, View} from 'react-native';
 import LoadingCheckView from './src/components/LoadingCheckView';
-
 import RNHighScores from './src/components/RNHighScores'
 import FlatListBasic from './src/components/FlatListBasic'
-import SettingsSectionLists from './src/components/SettingsSectionLists';
+import SectionListBasic from './src/components/SectionListBasic';
 import SettingTableView from './src/components/SettingTableView';
 
 
 AppRegistry.registerComponent('LoadingCheckView', () => LoadingCheckView);
-
-// Module name
 AppRegistry.registerComponent('RNHighScores', () => RNHighScores);
 AppRegistry.registerComponent('FlatListBasic', () => FlatListBasic);
-AppRegistry.registerComponent('SettingsSectionLists', () => SettingsSectionLists);
+AppRegistry.registerComponent('SectionListBasic', () => SectionListBasic);
 AppRegistry.registerComponent('SettingTableView', () => SettingTableView);
